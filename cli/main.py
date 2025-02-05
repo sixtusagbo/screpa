f252784fb91700106024495b25897e8d64066efb
Sixtus Agbo <miracleagbosixtus@gmail.com>
2025-02-05 20:50:28 +0100

    Add goodbye command

--- cli/main.py
+++ cli/main.py
@@ -12,5 +12,19 @@ def hello(name: str):
     print(f"Hello {name}")
 
 
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    """Say goodbye to an entity
+
+    Args:
+        name (str): Name of an entity
+        formal (bool): Formal or informal?
+    """
+    if formal:
+        print(f"Goodbye, Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
+
+
 if __name__ == "__main__":
     app()
